b7b3a153c84dfa21e4654e38a3799e9cd9eac69b
Yolandita <Yolandita@skytruth.org>
2014-04-03 11:55:09 -0400

    Modified to run on linux box

--- upload.py
+++ upload.py
@@ -11,10 +11,10 @@ import string
 
 #src = './tiffs/**/*.tiff'
 #src = '/cygdrive/i/NAIP/Pennsylvania/ByCounty/TOUPLOAD/**/*.sid'
-src = '//Fred/DataSets/NAIP/Pennsylvania/2013/TOUPLOAD/**/*.sid'
+src = '//media/sf_FRED_DATASETS/NAIP/Pennsylvania/2013/TOUPLOAD/**/*.sid'
 common_tags = ['autoupload:2014-03-14:01', 'Pennsylvania']
-key = './020ef2182a4b553b0a3381f1f849bf86a6e77862-privatekey.p12'
-uploader = '/home/kwurster/github/gme_tools/gme_tools/mapsengineupload.py'
+key = '../GME-PrivateKey/3bdd03d0a00be20af4c3138fad74fe2f9acd7259-privatekey.p12'
+uploader = 'gme_tools/mapsengineupload.py'
 
 
 src_files = glob.glob (src)
@@ -35,7 +35,6 @@ for s in src_files:
 		'--key=' + key,
 		s	
 	]
-#	print params	
 	call(params)
 	
 		
